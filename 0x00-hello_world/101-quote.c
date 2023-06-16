892a9822a824fbd6b19e31815f47f891b2d1bef8
hanaradwan <hana.ahmedradwan7@gmail.com>
2023-06-15 20:02:51 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
 * main - Entry point
 *
-* Description: A C program that prints with put function
+* Description:print a qoute using write function
 *
 *	ssize-t write (int fd.const void *buf.size_t count);
 *
-* Return: 1 (error)
+* Return: 1 (error).
 */
 
 int main(void)
