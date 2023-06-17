2cdc58c05f892726b7a13d3f605d96798bb0c64a
hanaradwan <hana.ahmedradwan7@gmail.com>
2023-06-16 18:28:35 -0700

    a

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -25,7 +25,7 @@ int main(void)
 				putchar((seconDigit / 10) + 48);
 				putchar((seconDigit % 10) + 48);
 
-				if (firstDigit != 98 && seconDigit != 99)
+				if (firstDigit != 98 || seconDigit != 99)
 				{
 					putchar(',');
 					putchar(' ');
