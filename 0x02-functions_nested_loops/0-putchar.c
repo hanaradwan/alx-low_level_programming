0c2f624950b93a24a07fd235aec803ae3a277d16
hanaradwan <hana.ahmedradwan7@gmail.com>
2023-06-20 05:53:48 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	chat str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+	return (0);
+}
